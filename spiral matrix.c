9ed56edec0c609aba807070b09da8157fe6b3df8
Aditya wadpalliwar <91073666+Adityawadpalliwar@users.noreply.github.com>
2024-11-20 10:14:18 +0000

    initial code

--- spiral matrix.c
+++ spiral matrix.c
@@ -8,17 +8,37 @@ int main()
 	int k=1;
 	printf("enter the value of n");
 	scanf("%d",&n);
+	int a[n][n];
 	
 	for(i=0;i<(n+1)/2;i++)
 	{
 		for(j=i;j<n-i;j++)
 		{
-			a[0][j-i]=k++;
+			a[i][j]=k++;
 		}
 		
-		for(j=i;j<n-i;j++)
+		for(j=i+1;j<n-i;j++)
 		{
-			a[j][n-i-1]
+			a[j][n-i-1]=k++;
+		}
+		for(j=n-2-i;j>=i;j--)
+		{
+			a[n-1-i][j]=k++;
+		}
+		for(j=n-2-i;j>i;j--)
+		{
+			a[j][i]=k++;
 		}
 	}
+
+	printf("now printing the matrix\n");
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%7d",a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
 }

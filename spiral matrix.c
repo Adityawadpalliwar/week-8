0f94cc607d97ead8cd7db4168104819aac89e278
Aditya wadpalliwar <91073666+Adityawadpalliwar@users.noreply.github.com>
2024-11-20 15:31:02 +0530

    Add files via upload

---
+++ spiral matrix.c
@@ -0,0 +1,24 @@
+//this is the program to print spiral matrix
+
+#include<stdio.h>
+
+int main()
+{
+	int n,i,j;
+	int k=1;
+	printf("enter the value of n");
+	scanf("%d",&n);
+	
+	for(i=0;i<(n+1)/2;i++)
+	{
+		for(j=i;j<n-i;j++)
+		{
+			a[0][j-i]=k++;
+		}
+		
+		for(j=i;j<n-i;j++)
+		{
+			a[j][n-i-1]
+		}
+	}
+}
